4b567a769704ef896029a2d2ae2089d2336a5969
HAV0X underscore <73144620+HAV0X1014@users.noreply.github.com>
2025-09-01 18:53:11 -0400

    PHP collatz yay

---
+++ php/collatz.php
@@ -0,0 +1,24 @@
+<?php
+$peak = 0;
+$steps = 0;
+
+echo("Enter a positive integer to run the collatz conjecture on: ");
+$number = (Int)readline();
+echo("Starting number: $number\n");
+$start_time = hrtime(true);
+while ($number != 1) {
+    if ($number > $peak) {
+        $peak = $number;
+    }
+    if ($number % 2 == 0) {
+        $number =  $number / 2;
+        $steps++;
+    } else {
+        $number = $number * 3 + 1;
+        $steps++;
+    }
+}
+$end_time = hrtime(true);
+$duration = ($end_time - $start_time) / 1e9;
+echo("Peak number: $peak Steps: $steps Duration: $duration seconds");
+?>
