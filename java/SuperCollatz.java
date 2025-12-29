b368e48a401a63a9e7f5300bce5090bbda26bc6f
HAV0X underscore <73144620+HAV0X1014@users.noreply.github.com>
2025-12-29 17:48:12 -0500

    keep the java version the same

--- java/SuperCollatz.java
+++ java/SuperCollatz.java
@@ -5,13 +5,6 @@
 
 import java.util.Scanner;
 
-/**
- * Written by HAV0X_ on 8/21/2025
- * Updated 8/26/2025
- */
-
-import java.util.Scanner;
-
 public class SuperCollatz {
     public static void main(String[] args) {
         int steps = 0;
@@ -21,15 +14,15 @@ public class SuperCollatz {
         System.out.println("Starting number: " + number);
         long startTime = System.nanoTime();
         while (number != 1) {
-            if ((number & 1) != 0) { //bitwise modulo
-                number = (3 * number) + 1;
-                steps++;
-            }
-            
             if (number > peak) peak = number;
-            
-            number >>= 1; //bitwise division by 2
-            steps++;
+            if ((number & 1) == 0) { //bitwise modulo
+                number >>= 1; //bitwise division by 2
+                steps++;
+            } else {
+                number = (3 * number) + 1;
+                number >>= 1;
+                steps += 2;
+            }
         }
         long endTime = System.nanoTime();
         System.out.println("Peak number: " + peak + " Steps: " + steps + " Calculation time: " + (endTime - startTime) + " nanoseconds.");
