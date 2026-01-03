c892e1ce0b7d98dd939429d111382e708ecdad9f
DrParanoya <158059657+DrParanoya@users.noreply.github.com>
2026-01-04 00:39:09 +0100

    Fix Java
    Fixed the broken peak logic and added credit for myself

--- java/SuperCollatz.java
+++ java/SuperCollatz.java
@@ -1,6 +1,7 @@
 /**
  * Written by HAV0X_ on 8/21/2025
  * Updated 8/26/2025
+ * Fixed by @DrParanoya on 12/29/2025
  */
 
 import java.util.Scanner;
@@ -14,15 +15,15 @@ public class SuperCollatz {
         System.out.println("Starting number: " + number);
         long startTime = System.nanoTime();
         while (number != 1) {
-            if (number > peak) peak = number;
-            if ((number & 1) == 0) { //bitwise modulo
-                number >>= 1; //bitwise division by 2
-                steps++;
-            } else {
+            if ((number & 1) != 0) { //bitwise modulo
                 number = (3 * number) + 1;
-                number >>= 1;
-                steps += 2;
+                steps++;
             }
+            
+            if (number > peak) peak = number;
+            
+            number >>= 1; //bitwise division by 2
+            steps++;
         }
         long endTime = System.nanoTime();
         System.out.println("Peak number: " + peak + " Steps: " + steps + " Calculation time: " + (endTime - startTime) + " nanoseconds.");
